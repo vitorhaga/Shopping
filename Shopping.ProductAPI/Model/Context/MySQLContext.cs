aa58db6e2296a47e36389265f639bc83d91752d6
vitorhaga <vitorhagamu@hotmail.com>
2024-09-18 19:45:30 -0300

    Adicionando url das imagens

--- Shopping.ProductAPI/Model/Context/MySQLContext.cs
+++ Shopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -24,7 +24,7 @@ namespace Shopping.ProductAPI.Model.Context
                 Name = "Camisa Slim Masculina Manga Comprida Marrom Xadrez",
                 Price = new decimal(223.3),
                 Description = "Camisa masculina social slim. Colarinho francês e manga comprida acabada em punho com botão. Fecho à frente com botões. Modelagem slim. Logo D bordado na altura do peito.",
-                ImageUrl = "",
+                ImageUrl = "https://github.com/vitorhaga/Shopping/blob/master/Shopping.Images/camisa_social_marrom_xadrez.jpg?raw=true",
                 CategoryName = "Camisa Social"
             });
             modelBuilder.Entity<Product>().HasData(new Product
@@ -33,7 +33,7 @@ namespace Shopping.ProductAPI.Model.Context
                 Name = "Blazer Feminino Alongado de Crepe e Botão Forrado Preto",
                 Price = new decimal(573.3),
                 Description = "Blazer feminino alongado e acinturado. Gola com lapela e manga comprida. Bolsos de aba na frente. Fecho frontal com 1 botão forrado. Interno forrado. Tecido leve, crepe.",
-                ImageUrl = "",
+                ImageUrl = "https://github.com/vitorhaga/Shopping/blob/master/Shopping.Images/blazer-feminino-alongado-de-crepe-e-botao-forrado-preto.jpg?raw=true",
                 CategoryName = "Blazer"
             });
             modelBuilder.Entity<Product>().HasData(new Product
@@ -42,7 +42,7 @@ namespace Shopping.ProductAPI.Model.Context
                 Name = "Calça Casual de Sarja Masculina Slim Areia",
                 Price = new decimal(459),
                 Description = "Calça casual em sarja masculina. Bolsos na frente e detalhe de bolsos embutidos atrás. Fecho frontal com zíper e botão. Cós com passantes. Modelo slim.",
-                ImageUrl = "",
+                ImageUrl = "https://github.com/vitorhaga/Shopping/blob/master/Shopping.Images/calca-casual-de-sarja-masculina-slim-areia.jpg?raw=true",
                 CategoryName = "Calça"
             });
 
